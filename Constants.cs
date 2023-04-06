e7337965b958de95b64a1654f26ec6d2643f7f66
ciklosport <ciklosport.zagreb@gmail.com>
2023-04-06 12:36:45 +0200

    tonko <3

--- Constants.cs
+++ Constants.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using System.IO;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace ciklonalozi
 {
     public static class C
     {
+        public static readonly TimeZoneInfo TZ = TimeZoneInfo.FindSystemTimeZoneById("Europe/Zagreb");
+        public static readonly CultureInfo CI = CultureInfo.GetCultureInfo("hr-HR");
         public static class Hasher
         {
             public static readonly Hashids Ids = new(Env.SALT, 4, Env.ALPHABET);
--- Pages/Print.cshtml.cs
+++ Pages/Print.cshtml.cs
@@ -1,6 +1,6 @@
+using System;
 using System.Threading.Tasks;
 using ciklonalozi.Data;
-using HashidsNet;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
@@ -22,6 +22,18 @@ namespace ciklonalozi.Pages
 
             using var db = _dbFactory.CreateDbContext();
             Order = await db.Orders.FindAsync(id);
+
+            if (Order == null)
+                return;
+
+            // Adjust for TZ
+            Order.Arrival = TimeZoneInfo.ConvertTimeFromUtc(Order.Arrival, C.TZ);
+            if (Order.Arrived.HasValue)
+                Order.Arrived = TimeZoneInfo.ConvertTimeFromUtc(Order.Arrived.Value, C.TZ);
+            if (Order.Completed.HasValue)
+                Order.Completed = TimeZoneInfo.ConvertTimeFromUtc(Order.Completed.Value, C.TZ);
+            if (Order.Returned.HasValue)
+                Order.Returned = TimeZoneInfo.ConvertTimeFromUtc(Order.Returned.Value, C.TZ);
         }
     }
 }
